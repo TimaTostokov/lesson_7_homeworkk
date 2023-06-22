21a6a6f66c9ae2ebd9db1eb2c8f1a48672026ead
TimaTostokov <t1005828@gmail.com>
2023-06-22 17:11:14 +0300

    ДЗ на сообразительность не сделал

---
+++ src/Medic.java
@@ -0,0 +1,7 @@
+public class Medic extends Hero {
+    public int healPoints;
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic применил суперспособность treatment");
+    }
+}
